61ec7eaef72a1ee430234eb50a06f3855a8b8b68
Kira <kkovaluk11@gmail.com>
2026-03-06 09:37:49 +0300

    +

--- C#CadAI/CadGenerator/CadGenerator/obj/Debug/net9.0/CadGenerator.AssemblyInfo.cs
+++ C#CadAI/CadGenerator/CadGenerator/obj/Debug/net9.0/CadGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2af484141db6f534c5af30c20ed1f87467ff67")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
